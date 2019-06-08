9485d2db022fd6a9a4888d453d84b892b895ea9d
Akanshu <akanshuraj@gmail.com>
2019-06-08 13:52:21 +0530

    stateless functional components1

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -20,15 +20,6 @@ function ListContacts (props) {
 
 class ListContacts extends Component{
   render() {
-    return(
-      <ol className='contact-list'>
-        {this.props.contacts.map((contact) => (
-          <li key={contact.id}>
-            {contact.name}
-          </li>
-        ))}
-      </ol>
-    )
   }
 }
 
