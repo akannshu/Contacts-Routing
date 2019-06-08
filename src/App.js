f624b2d8d694d61ded579965b8dbc9fc4183831a
Akanshu <akanshuraj@gmail.com>
2019-06-08 16:43:33 +0530

    propTypes

--- src/App.js
+++ src/App.js
@@ -33,7 +33,10 @@ class App extends Component {
   render(){
     return(
       <div className="App">
-        <ListContacts onDeleteConatact={this.removeContact} contacts={this.state.contacts}/>
+        <ListContacts
+          onDeleteConatact={this.removeContact}
+          contacts={this.state.contacts}
+          />
       </div>
     )
   }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 function ListContacts (props) {
  return (
@@ -18,6 +19,11 @@ function ListContacts (props) {
      ))}
    </ol>
  )
+}
+
+ListContacts.propTypes = {
+   contacts: PropTypes.array.isRequired,
+   onDeleteConatact: PropTypes.func.isRequired
 }
 
 export default ListContacts
