2647dc0799110ef4d9c834d251345162555d0114
Akanshu <akanshuraj@gmail.com>
2019-06-09 01:23:49 +0530

    form layout complete

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import ImageInput from './ImageInput'
+import { Link } from 'react-router-dom'
 
 class CreateContact extends Component {
   render() {
     return (
-      <div>Create Contact!</div>
+      <div>
+        <Link to='/' className="close-create-contact">Close</Link>
+        <form className="create-contact-form">
+          <ImageInput
+            className="create-contact-avatar-input"
+            name = "avatarURL"
+            maxHeight={64}
+            />
+          <div className="create-contact-details">
+            <input type="text" name="name" placeholder="Name"></input>
+            <input type="email" name="email" placeholder="Email"></input>
+            <button>Add Contact</button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
