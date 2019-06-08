c3f975114787b04567f4ca619906e3e8fe3b4cf9
Akanshu <akanshuraj@gmail.com>
2019-06-09 01:58:11 +0530

    form complete

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import ImageInput from './ImageInput'
 import { Link } from 'react-router-dom'
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const values = serializeForm(e.target, { hash:true })
+    console.log(values);
+  }
   render() {
     return (
       <div>
         <Link to='/' className="close-create-contact">Close</Link>
-        <form className="create-contact-form">
+        <form onSubmit={this.handleSubmit} className="create-contact-form">
           <ImageInput
             className="create-contact-avatar-input"
             name = "avatarURL"
